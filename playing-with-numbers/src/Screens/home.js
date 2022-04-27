0388e9da081c0d62b8e079270e94eeddf278b67f
Mariana Saraiva <marianascm8@gmail.com>
2022-04-27 17:15:40 -0300

    feat: create Home screen

---
+++ playing-with-numbers/src/Screens/home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import ImageScreen from '../image/background.jpg';
+import Button from '../Components/button';
+
+
+export default function Home({ navigation }) {
+
+  const redirectLogin = () => {
+    navigation.navigate('Login');
+  };
+
+  const redirectRegister = () => {
+    navigation.navigate('Register');
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={ImageScreen}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>
+          Are not registered, click here!
+        </Text>
+        <Button onPress={redirectLogin} title="Login" />
+
+        <Button onPress={redirectRegister} title="Register" />
+
+        <StatusBar style="auto" />
+
+      </ImageBackground>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    backgroundColor: 'black',
+    color: 'white',
+    textAlign: 'center',
+  },
+});
