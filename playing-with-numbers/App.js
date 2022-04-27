d19fbb3aa5cf79823f64eeb84ec8bbb056346839
Taina Santana <tainnaps@gmail.com>
2022-04-26 22:22:13 -0300

    feat: use react navigation and screens

--- playing-with-numbers/App.js
+++ playing-with-numbers/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/Pages/login';
+import Game from './src/Pages/game';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Login/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          // options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen
+          name="Game"
+          component={Game}
+          // options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
