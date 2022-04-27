efb250788bfebb5566ec4c3597c7bcf67265709f
Mariana Saraiva <marianascm8@gmail.com>
2022-04-27 17:18:27 -0300

    feat: add home and register routes

--- playing-with-numbers/src/routes/index.js
+++ playing-with-numbers/src/routes/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../Screens/login';
 import Game from '../Screens/game';
+import Register from '../Screens/register';
+import Home from '../Screens/home';
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+      />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+      />
       <Stack.Screen
         name="Login"
         component={Login}
