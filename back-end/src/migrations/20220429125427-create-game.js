e7a8093dda17372377db25b8697b4b2b1bdb715a
Mariana Saraiva <marianascm8@gmail.com>
2022-04-29 11:13:02 -0300

    feat: create database migrations

---
+++ back-end/src/migrations/20220429125427-create-game.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Games');
+  }
+};
