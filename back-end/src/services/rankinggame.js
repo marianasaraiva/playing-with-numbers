439f638bd8320ae1ba888299147690813e31ab5b
Taina Santana <tainnaps@gmail.com>
2022-05-04 20:49:59 -0300

    feat: include avatar url to ranking

--- back-end/src/services/rankinggame.js
+++ back-end/src/services/rankinggame.js
@@ -1,4 +1,4 @@
-const { User, RankingGame } = require('../models');
+const { User, Avatar, RankingGame } = require('../models');
 
 const getByGameId = async (id) => {
   const ranking = await RankingGame.findAll({
@@ -6,8 +6,16 @@ const getByGameId = async (id) => {
     include: [
       {
         model: User,
-        as: 'users'
-      }
+        as: 'users',
+        attributes: { exclude: ['password', 'id'] },
+        include: [
+          {
+            model: Avatar,
+            as: 'avatar',
+            attributes: { exclude: ['name', 'id'] },
+          },
+        ],
+      },
     ],
     order: [['score', 'DESC']],
     limit: 10,
