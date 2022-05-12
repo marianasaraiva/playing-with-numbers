892f8ae99e2f2f00561fd1d2696a49081b6ca050
Mariana Saraiva <marianascm8@gmail.com>
2022-05-12 12:47:22 -0300

    feat: add games picker

--- front-end/src/Screens/game.js
+++ front-end/src/Screens/game.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Picker } from 'react-native';
+import { fetchAPIGet } from '../services/fetchAPI';
 
 export default function Game() {
+  const [selectedGame, setSelectedGame] = useState('Sum');
+  const [games, setGames] = useState([]);
+  
+  const fetchAPIGame = async () => {
+    const result = await fetchAPIGet('get', 'http://localhost:3001/game');
+    setGames(result);
+  }
+
+  useEffect(() => {
+    fetchAPIGame();
+  }, [])
+
   return (
     <View>
-      <Text>
-        Game
-      </Text>
+      <Text>Game de Matemática</Text>
+      <Picker
+        selectedValue={selectedGame}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(game) => setSelectedGame(game)}
+      >
+        {
+          games.length !== 0
+            ? games.map(({ name, id }) => (
+              <Picker.Item key={id} label={name} value={name} /> ))
+            : null
+        }
+      </Picker>
     </View>
   );
 }
