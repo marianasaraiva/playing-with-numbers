a9477816a5e51f3a4438bf1feb1569b0ae6b7803
Taina Santana <tainnaps@gmail.com>
2022-05-16 19:01:45 -0300

    feat: add play button

--- front-end/src/Screens/game.js
+++ front-end/src/Screens/game.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import Context from '../context/Context';
+import Button from '../Components/button';
 import { View, Text, Picker } from 'react-native';
 import { fetchAPIGet } from '../services/fetchAPI';
 
 export default function Game() {
+  const { setGame } = useContext(Context);
   const [selectedGame, setSelectedGame] = useState('Sum');
   const [games, setGames] = useState([]);
-  
+
   const fetchAPIGame = async () => {
     const result = await fetchAPIGet('get', 'http://localhost:3001/game');
     setGames(result);
@@ -14,6 +17,8 @@ export default function Game() {
   useEffect(() => {
     fetchAPIGame();
   }, [])
+
+  
 
   return (
     <View>
@@ -30,6 +35,9 @@ export default function Game() {
             : null
         }
       </Picker>
+      <Button onPress={ () => setGame(selectedGame) } title="Jogar"/>
+      {/* <Button onPress={} title="Ranking"/>
+      <Button onPress={} title="Level"/> */}
     </View>
   );
 }
