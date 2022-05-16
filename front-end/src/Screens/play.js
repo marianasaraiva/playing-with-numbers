f9f323e340909b724742be0b5d72e17204471b75
Taina Santana <tainnaps@gmail.com>
2022-05-16 19:01:11 -0300

    feat: add provider and play route

---
+++ front-end/src/Screens/play.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import Context from '../context/Context';
+
+export default function Play() {
+  const { game } = useContext(Context);
+  return (
+    <View>
+      <Text>
+        {`Play ${game}`}
+      </Text>
+    </View>
+  )
+}
--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,31 +1,39 @@
 import React from 'react';
+import Provider from '../context/Provider';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../Screens/login';
 import Game from '../Screens/game';
 import Register from '../Screens/register';
 import Home from '../Screens/home';
+import Play from '../Screens/play';
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen
-        name="Home"
-        component={Home}
-      />
-      <Stack.Screen
-        name="Register"
-        component={Register}
-      />
-      <Stack.Screen
-        name="Login"
-        component={Login}
-      />
-      <Stack.Screen
-        name="Game"
-        component={Game}
-      />
+      <Provider>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          name="Game"
+          component={Game}
+        />
+        <Stack.Screen
+          name="Play"
+          component={Play}
+        />
+      </Provider>
   </Stack.Navigator>
   )
 }
