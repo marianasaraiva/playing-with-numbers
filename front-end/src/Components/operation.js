13215e65e957b7adaf222c253811f2884d61b70d
Mariana Saraiva <marianascm8@gmail.com>
2022-05-23 22:03:19 -0300

    feat: dinamic table

--- front-end/src/Components/operation.js
+++ front-end/src/Components/operation.js
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 
-const tableData = [
-  ['1', 'x', '2', '=', ''],
-  ['2', 'x', '2', '=', ''],
-  ['3', 'x', '2', '=', ''],
-  ['4', 'x', '2', '=', ''],
-  ['5', 'x', '2', '=', ''],
-  ['6', 'x', '2', '=', ''],
-  ['7', 'x', '2', '=', ''],
-  ['8', 'x', '2', '=', ''],
-  ['9', 'x', '2', '=', ''],
-  ['10', 'x', '2', '=', ''],
-];
+const tableData = [];
 
-const tableHead = ['Head', 'Head2', 'Head3', 'Head4', 'Head5'];
+const tableHead = ['Número', 'Operação', 'Número', 'Igual a'];
 
 export default function OperationTable() {
   const [data, setData] = useState({tableHead, tableData});
+  // const [randomNumber, setRandom] = useState(1);
+  const [response, setResponse] = useState([]);
+  const [userResponse, setUserResponse] = useState('');
 
+  const getRandomNumber = () => { 
+    return Math.floor(Math.random() * (10 - 1)) + 1;
+  }
+  
+  const calculate = (num) => {
+    const result = [];
+    for (let x = 1; x <= 10; x += 1) {
+      tableData.push([`${num}`, '*', `${x}`, '=']);
+      setData({...data, tableData });
+      result.push(num * x);
+      setResponse(result);
+    };
+  };
+  
+  useEffect(() => { calculate(getRandomNumber()) }, []);
+  
   return (
     <View style={styles.container}>
       <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
         <Row data={data.tableHead} style={styles.head} textStyle={styles.text}/>
         <Rows data={data.tableData} textStyle={styles.text}/>
       </Table>
+      { 
+        response.length === 10 && response.map((row, index) => (
+          <TextInput
+            key={index}
+            onChangeText={setUserResponse}
+            placeholder="?"
+            value={userResponse}
+            name={index}
+            /> 
+        ))
+      }
     </View>
   )
 }
