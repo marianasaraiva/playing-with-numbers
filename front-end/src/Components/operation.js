7db1877a5263bd03805596c5e958f39a1b130ed7
Mariana Saraiva <marianascm8@gmail.com>
2022-06-05 22:29:36 -0300

    feat: add check logic result

--- front-end/src/Components/operation.js
+++ front-end/src/Components/operation.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
+import Button from './button';
 
 const tableData = [];
 
 const tableHead = ['Número', 'Operação', 'Número', 'Igual a'];
 
-export default function OperationTable() {
+export default function OperationTable({ navigation }) {
   const [data, setData] = useState({tableHead, tableData});
-  // const [randomNumber, setRandom] = useState(1);
   const [response, setResponse] = useState([]);
-  const [userResponse, setUserResponse] = useState('');
+  const [userResponse, setUserResponse] = useState(new Array(10).fill(''));
+  const [disabled, setDisabled] = useState(true);
+  const [showResult, setShowResult] = useState(false);
 
   const getRandomNumber = () => { 
     return Math.floor(Math.random() * (10 - 1)) + 1;
@@ -28,23 +30,57 @@ export default function OperationTable() {
   
   useEffect(() => { calculate(getRandomNumber()) }, []);
   
+  const handleOnChange = (value, position) => {
+    const updatedCheckedState = userResponse.map((item, index) =>
+      index === position ? Number(value) : item
+    );
+    setUserResponse(updatedCheckedState);
+    const checkAnswer = userResponse
+      .map((answer) => answer !== '')
+      .every((answer) => answer === true);
+    checkAnswer && setDisabled(false);
+  };
+
+  const onClick = () => {
+    const checkAnswer = response
+    .map((answer, index) => answer === userResponse[index])
+    .every((answer) => answer === true);
+    checkAnswer && setShowResult(true)
+  };
+
+  const nextNumber = () => navigation.navigate('Game');
+
   return (
     <View style={styles.container}>
       <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
         <Row data={data.tableHead} style={styles.head} textStyle={styles.text}/>
         <Rows data={data.tableData} textStyle={styles.text}/>
       </Table>
-      { 
-        response.length === 10 && response.map((row, index) => (
-          <TextInput
-            key={index}
-            onChangeText={setUserResponse}
-            placeholder="?"
-            value={userResponse}
-            name={index}
-            /> 
-        ))
+      {
+        showResult 
+        ?
+          <Text>Você ganhou!!! :palmas:</Text>
+        :
+          <View>
+            { 
+              response.length === 10 && response.map((_row, index) => (
+                <TextInput
+                  key={ index }
+                  onChangeText={ (target) => handleOnChange(target, index) }
+                  placeholder="?"
+                  value={ userResponse[index] }
+                  name={ index }
+                /> 
+              ))
+            }
+          </View>
       }
+      <Button
+        onPress={ showResult ? nextNumber : onClick }
+        title={ showResult ? "Próximo" : "Finalizar" }
+        disabled={ disabled }
+      />
+
     </View>
   )
 }
