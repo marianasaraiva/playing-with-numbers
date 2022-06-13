aa8d73cda67b3a0a476fc62781d0140d831971f9
Tainá Santana <tainnaps@gmail.com>
2022-06-13 20:10:08 -0300

    feat: add token and user to provider

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -4,10 +4,16 @@ import Context from './Context';
 
 export default function Provider({ children }) {
   const [game, setGame] = useState('Sum');
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState('');
 
   const context = {
     game,
     setGame,
+    token,
+    setToken,
+    user,
+    setUser,
   }
 
   return (
