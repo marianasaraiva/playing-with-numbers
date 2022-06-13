76df47235c28f13a65d76587a2c65a4e9f7e92be
Tainá Santana <tainnaps@gmail.com>
2022-06-13 18:18:52 -0300

    refactor: switch table for react native text

--- front-end/src/Components/operation.js
+++ front-end/src/Components/operation.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { StyleSheet, View, TextInput, Text } from 'react-native';
-import { Table, Row, Rows } from 'react-native-table-component';
 import Button from './button';
 import Context from '../context/Context';
 
-const tableData = [];
-
-const tableHead = ['Número', 'Operação', 'Número', 'Igual a'];
-
 export default function OperationTable({ navigation }) {
   const { game } = useContext(Context);
-  const [data, setData] = useState({tableHead, tableData});
+  const [data, setData] = useState([]);
   const [response, setResponse] = useState([]);
   const [userResponse, setUserResponse] = useState(new Array(10).fill(''));
-  const [disabled, setDisabled] = useState(true);
   const [showResult, setShowResult] = useState(false);
+  const [finishMessage, setFinishMessage] = useState('');
 
   const getRandomNumber = () => { 
     return Math.floor(Math.random() * (10 - 1)) + 1;
@@ -35,6 +30,7 @@ export default function OperationTable({ navigation }) {
   
   const calculate = (num) => {
     const result = [];
+    const row = [];
     const operations = {
       Sum: '+',
       Substraction: '-',
@@ -43,8 +39,8 @@ export default function OperationTable({ navigation }) {
     };
 
     for (let x = 1; x <= 10; x += 1) {
-      tableData.push([`${x}`,`${operations[game]}`, `${num}`, '=']);
-      setData({...data, tableData });
+      row.push(`${x} ${operations[game]} ${num} =`);
+      setData(row);
       result.push(getResult(num, x, operations[game]));
       setResponse(result);
     };
@@ -57,50 +53,44 @@ export default function OperationTable({ navigation }) {
       index === position ? Number(value) : item
     );
     setUserResponse(updatedCheckedState);
-    const checkAnswer = userResponse
-      .map((answer) => answer !== '')
-      .every((answer) => answer === true);
-    checkAnswer && setDisabled(false);
   };
 
   const onClick = () => {
-    const checkAnswer = response
-    .map((answer, index) => answer === userResponse[index])
-    .every((answer) => answer === true);
-    checkAnswer && setShowResult(true)
+    const checkAnswer = response.every((answer, index) => answer === userResponse[index]);
+    setShowResult(true);
+    checkAnswer
+      ? setFinishMessage('Você ganhou!!!')
+      : setFinishMessage('Vamos treinar um pouco mais?');
   };
 
   const nextNumber = () => navigation.navigate('Game');
 
   return (
     <View style={styles.container}>
-      <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-        <Row data={data.tableHead} style={styles.head} textStyle={styles.text}/>
-        <Rows data={data.tableData} textStyle={styles.text}/>
-      </Table>
       {
         showResult 
         ?
-          <Text>Você ganhou!!! :palmas:</Text>
+          <Text>{ finishMessage }</Text>
         :
           <View>
             { 
-              response.length === 10 && response.map((_row, index) => (
-                <TextInput
-                  key={ index }
-                  onChangeText={ (target) => handleOnChange(target, index) }
-                  placeholder="?"
-                  value={ userResponse[index] }
-                  name={ index }
-                /> 
-              ))
+              response.length === 10 && data.map((row, index) => (
+                <View key={ index }>
+                  <Text style={styles.text}>{ row }</Text>
+                  <TextInput
+                    onChangeText={ (target) => handleOnChange(target, index) }
+                    placeholder="?"
+                    value={ userResponse[index] }
+                    name={ index }
+                  /> 
+                </View>)
+              )
             }
           </View>
       }
       <Button
         onPress={ showResult ? nextNumber : onClick }
         title={ showResult ? "Próximo" : "Finalizar" }
-        disabled={ disabled }
       />
 
     </View>
@@ -108,7 +98,6 @@ export default function OperationTable({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
+  container: { flex: 1, padding: 16, paddingTop: 30 },
   text: { margin: 6 }
 });
